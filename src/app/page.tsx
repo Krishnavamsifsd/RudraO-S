77f6ae493ef6db63f077a75769c4bba3c0b367b8
krishnavamsi-37 <krishna.v@twinleaves.co>
2024-08-30 02:14:19 +0530

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import AboutSectionOne from "@/components/About/AboutSectionOne";
 import AboutSectionTwo from "@/components/About/AboutSectionTwo";
-import Blog from "@/components/Blog";
 import ScrollUp from "@/components/Common/ScrollUp";
 import Contact from "@/components/Contact";
 import Features from "@/components/Features";
